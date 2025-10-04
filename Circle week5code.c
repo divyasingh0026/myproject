4c07bf450c78e499837c74bf804ad43627da1167
divyasingh0026 <daisyworlddivya@gmail.com>
2025-10-04 20:17:31 +0530

    commit

---
+++ Circle week5code.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	int radius,area;
+	float perimeter;
+	printf("the radius of circle is \n");
+	scanf("%d",&radius);
+			printf("the area of circle is %d \n", area=3.14*radius*radius);
+			
+	printf("now the perimeter of circle is\n");
+	
+		printf("the radius of circle is \n");
+	scanf("%d",&radius);
+	perimeter= 2*3.14*radius;
+		printf("the perimeter of circle is %f \n",perimeter);
+	return 0;
+}
