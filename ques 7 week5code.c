311fcba1cb918c75e8129665656911737dd4a729
divyasingh0026 <daisyworlddivya@gmail.com>
2025-10-05 18:47:30 +0530

    commit

---
+++ ques 7 week5code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	char A,a,e,i,o,u;
+	
+		printf("program for determining an alphabet 'A' is vowel or consonant \n");
+		
+
+	
+	printf("enter any alphabet'A'\n ");
+scanf ("%c",&A);
+
+
+if( A =='a'|| A=='e'|| A=='i'|| A=='o'|| A=='u'||A =='A'|| A=='E'|| A=='I'|| A=='O'|| A=='U'){
+	printf(" the alphabet is vowel\n")	;
+}else{
+printf(" the alphabet is consonant\n");
+}
+	return 0;
+}
